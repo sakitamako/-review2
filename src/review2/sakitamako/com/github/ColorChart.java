cd1dcdb9ee0006af93d30001e36a5594ceef1b16
sakitamako <sa.919.1025.ki@gmail.com>
2024-04-09 20:57:40 +0900

    インスタンス化

--- src/review2/sakitamako/com/github/ColorChart.java
+++ src/review2/sakitamako/com/github/ColorChart.java
@@ -1,5 +1,11 @@
 package review2.sakitamako.com.github;
 
 public class ColorChart {
-
+	public String color = null;
+	public int level = 0;
+	public String length = null;
+	
+	public void style() {
+		System.out.println(this.length + "スタイルで" + this.color + "カラーの仕上がり");
+	}
 }
--- src/review2/sakitamako/com/github/ColorTest.java
+++ src/review2/sakitamako/com/github/ColorTest.java
@@ -1,5 +1,27 @@
 package review2.sakitamako.com.github;
 
 public class ColorTest {
+	public static void main(String[] args) {
+		ColorChart red = new ColorChart();
+		red.color = "赤";
+		red.level = 7;
+		red.length = "ミディアム";
+		
+		System.out.println(red.color);
+		System.out.println(red.level);
+		System.out.println(red.length);
+		
+		ColorChart blue = new ColorChart();
+		blue.color = "青";
+		blue.level = 5;
+		blue.length = "ロング";
+		
+		System.out.println(blue.color);
+		System.out.println(blue.level);
+		System.out.println(blue.length);
+		
+		red.style();
+		blue.style();
+	}
 
 }
